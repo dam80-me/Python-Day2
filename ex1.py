dd1e5c85de073a19a97b24a4573b9b8113673e13
dam80-me <malinda_madurawela@yahoo.com>
2025-06-20 03:12:47 -0700

    third commit

--- ex1.py
+++ ex1.py
@@ -1,8 +1,10 @@
 # Create the list named temperatures
 temperatures =[25.5,28.1,22.9,30.2,27.8,24.3, 29.5]
 
+print(temperatures)
 #Add 31.0 to the end of the temperature list
 temperatures.append(31.0)
+print(temperatures)
 
 #Remove the third element (index 2) from the list
 temp = temperatures.pop(2)
@@ -16,7 +18,7 @@ min_temperature = min(temperatures)
 average_temperature = sum(temperatures)/len(temperatures)
 
 #Print the modofied list, the maximum, minimum, and average temperature
-print(f"Modified temperatures list: {max_temperature}")
+print(f"Modified temperatures list: {temperatures}")
 print(f"Maximum temperatures list: {max_temperature}")
-print(f"Minimum temperatures list: {max_temperature}")
+print(f"Minimum temperatures list: {min_temperature}")
 print(f"Average temperatures list: {average_temperature: .2f}")
