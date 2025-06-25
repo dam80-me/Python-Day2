4cd0bb0b0894bd8e020c4a7e7fadb7a36778fab2
dam80-me <malinda_madurawela@yahoo.com>
2025-06-25 02:25:30 -0700

    11th commit

---
+++ ex10.py
@@ -0,0 +1,12 @@
+# Create the list named temperatures
+temperatures = [25.5, 28.1, 22.9,30.2, 27.8, 24.3, 29.5]
+test []
+
+numbers = [i for i in range (1,11)]
+print(f"{numbers}")
+
+# Add 31.0 to the end of the temperatures list
+temperatures.append(31.0)
+
+# Remove the third element (index 2)  from the list
+temp print(temp)
